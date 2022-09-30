8f643074bc8543fdb2e08f9778629af422c711a9
DohBae <bdoherty92@msn.com>
2022-09-30 15:08:30 +0000

    Seed notes

--- LHL-Final-Project/server/db/seeds/01_users.sql
+++ LHL-Final-Project/server/db/seeds/01_users.sql
@@ -1,13 +1,10 @@
 INSERT INTO users (id, firstName, lastName, email, password, phoneNumber, profilePic, note_id ) 
 VALUES (1, Netsanet, Asfaw, netsi@gmail.com, 1234, 4372552020, 
-https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.designyourway.net%2Fblog%2Finspiration%2Fwallpapers%2Fcute-wallpapers%2F&psig=AOvVaw2TV9_CfocLZqDHKzIhLuV8&ust=1664635502030000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiz2c_gvPoCFQAAAAAdAAAAABAE, 
-3);
+https://images-na.ssl-images-amazon.com/images/I/71kkMXAcLCL.png, 3);
 
 INSERT INTO users (id, firstName, lastName, email, password, phoneNumber, profilePic, note_id ) 
-VALUES (2, Fatima, Khan, fatimak@gmail.com, 1234, 6474769087, 
-https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaperaccess.com%2Fcute-hd&psig=AOvVaw2TV9_CfocLZqDHKzIhLuV8&ust=1664635502030000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiz2c_gvPoCFQAAAAAdAAAAABAJ3);
+VALUES (2, Fatima, Khan, fatimak@gmail.com, 1234, 6474769087, https://i.imgur.com/2Bb4yfw.jpg, 2);
 
 INSERT INTO users (id, firstName, lastName, email, password, phoneNumber, profilePic, note_id ) 
 VALUES (3, Bailee, Doherty, bdoherty@finalproject.ca, 1234, 5555555555, 
-https://www.google.com/url?sa=i&url=https%3A%2F%2Ffininfoo.com%2Fnews%2F1708%2Fiphone-wallpaper-hd-girly-cute-girly-iphone-wallpapers&psig=AOvVaw2TV9_CfocLZqDHKzIhLuV8&ust=1664635502030000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiz2c_gvPoCFQAAAAAdAAAAABAO3);
-
+https://www.designyourway.net/blog/wp-content/uploads/2018/08/387011_3d-cute-wallpapers-for-desktop-hd-1-jpg_1024x768_h-700x525.jpg, 1);
