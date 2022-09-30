c66e113beb7d58f8b5e633fce3a6d5146c30a238
DohBae <bdoherty92@msn.com>
2022-09-30 15:15:41 +0000

    Seed classes

--- LHL-Final-Project/server/db/seeds/02_notes.sql
+++ LHL-Final-Project/server/db/seeds/02_notes.sql
@@ -1,5 +1,5 @@
 INSERT INTO notes (id, title, body, picture, publishDate, user_id, class_id, semester_id)
-VALUES (1,"Chickamauga", "One sunny autumn afternoon a child strayed away from its rude home in a small field and entered a forest unobserved. It was happy in a new sense of freedom from control, happy in the opportunity of exploration and adventure; for this child's spirit, in bodies of its ancestors, had for thousands of years been trained to memorable feats of discovery and conquest--victories in battles whose critical moments were centuries, whose victors' camps were cities of hewn stone. From the cradle of its race it had conquered its way through two continents and passing a great sea had penetrated a third, there to be born to war and dominion as a heritage.","", current_timestamp, 1, 2, 2);
+VALUES (1,"Chickamauga", "One sunny autumn afternoon a child strayed away from its rude home in a small field and entered a forest unobserved. It was happy in a new sense of freedom from control, happy in the opportunity of exploration and adventure; for this child's spirit, in bodies of its ancestors, had for thousands of years been trained to memorable feats of discovery and conquest--victories in battles whose critical moments were centuries, whose victors' camps were cities of hewn stone. From the cradle of its race it had conquered its way through two continents and passing a great sea had penetrated a third, there to be born to war and dominion as a heritage.","", current_timestamp, 1, 1, 1);
 
 INSERT INTO notes (id, title, body, picture, publishDate, user_id, class_id, semester_id)
 VALUES (2, 'Still I Rise by Maya Angelou', "You may write me down in history
@@ -52,9 +52,9 @@ Bringing the gifts that my ancestors gave,
 I am the dream and the hope of the slave.
 I rise
 I rise
-I rise.", '' , current_timestamp, 2, 2, 4 );
+I rise.", '' , current_timestamp, 2, 2, 2 );
 
 INSERT INTO notes (id, title, body, picture, publishDate, user_id, class_id, semester_id)
 VALUES (3, "Tragedy of Darth Plagueis (Stories the Jedi wont tell you)", "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. Its not a story the Jedi would tell you. Its a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.",
 https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Fmobile%2F000%2F031%2F213%2Fironic.jpg, 
-current_timestamp, 3, 2, 3);
+current_timestamp, 3, 3, 3);
