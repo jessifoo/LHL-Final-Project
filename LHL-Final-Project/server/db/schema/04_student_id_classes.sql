928207544417a602fccf95bae5f31764d61e444f
DohBae <bdoherty92@msn.com>
2022-09-30 14:11:25 +0000

    Add semesters table to database

--- LHL-Final-Project/server/db/schema/04_student_id_classes.sql
+++ LHL-Final-Project/server/db/schema/04_student_id_classes.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS classes CASCADE;
+DROP TABLE IF EXISTS student_id_class CASCADE;
 CREATE TABLE student_id_class (
   student_id INTEGER REFERENCES student(id) ON DELETE CASCADE,
   class_id INTEGER REFERENCES class(id) ON DELETE CASCADE,
