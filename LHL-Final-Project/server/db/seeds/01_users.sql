d74207acc53f8b0a8b8a8097226f34a3279c3e02
DohBae <bdoherty92@msn.com>
2022-09-30 14:50:34 +0000

    Seed users

---
+++ LHL-Final-Project/server/db/seeds/01_users.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (id, firstName, lastName, email, password, phoneNumber, profilePic, note_id ) 
+VALUES (1, Netsanet, Asfaw, netsi@gmail.com, 1234, 4372552020, 
+https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.designyourway.net%2Fblog%2Finspiration%2Fwallpapers%2Fcute-wallpapers%2F&psig=AOvVaw2TV9_CfocLZqDHKzIhLuV8&ust=1664635502030000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiz2c_gvPoCFQAAAAAdAAAAABAE, 
+3);
+
+INSERT INTO users (id, firstName, lastName, email, password, phoneNumber, profilePic, note_id ) 
+VALUES (2, Fatima, Khan, fatimak@gmail.com, 1234, 6474769087, 
+https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaperaccess.com%2Fcute-hd&psig=AOvVaw2TV9_CfocLZqDHKzIhLuV8&ust=1664635502030000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiz2c_gvPoCFQAAAAAdAAAAABAJ3);
+
+INSERT INTO users (id, firstName, lastName, email, password, phoneNumber, profilePic, note_id ) 
+VALUES (3, Bailee, Doherty, bdoherty@finalproject.ca, 1234, 5555555555, 
+https://www.google.com/url?sa=i&url=https%3A%2F%2Ffininfoo.com%2Fnews%2F1708%2Fiphone-wallpaper-hd-girly-cute-girly-iphone-wallpapers&psig=AOvVaw2TV9_CfocLZqDHKzIhLuV8&ust=1664635502030000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiz2c_gvPoCFQAAAAAdAAAAABAO3);
+
