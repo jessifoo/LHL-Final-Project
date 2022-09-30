0e6abff0bec42a7f9c85f2ee6d9f663809d2498e
DohBae <bdoherty92@msn.com>
2022-09-30 14:18:01 +0000

    Add favourites table to database

--- LHL-Final-Project/server/db/schema/04_student_id_classes.sql
+++ LHL-Final-Project/server/db/schema/04_student_id_classes.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS student_id_class CASCADE;
 CREATE TABLE student_id_class (
-  student_id INTEGER REFERENCES student(id) ON DELETE CASCADE,
-  class_id INTEGER REFERENCES class(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  class_id INTEGER REFERENCES classes(id) ON DELETE CASCADE,
 );
