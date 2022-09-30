b1be7645a1af2c35e044341f3337c59850c5373b
DohBae <bdoherty92@msn.com>
2022-09-30 15:23:08 +0000

    Seed ratings

---
+++ LHL-Final-Project/server/db/seeds/06_ratings.sql
@@ -0,0 +1,8 @@
+INSERT INTO ratings (id, rating, note_id )
+VALUES (1, 5, 1);
+
+INSERT INTO ratings (id, rating, note_id )
+VALUES (2, 4, 2);
+
+INSERT INTO ratings (id, rating, note_id )
+VALUES (3, 3, 3);
