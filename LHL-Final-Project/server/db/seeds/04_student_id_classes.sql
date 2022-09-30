fcf744f3a21f436a254b7b2e2525b838cfa1d8b6
DohBae <bdoherty92@msn.com>
2022-09-30 15:18:25 +0000

    Seed student_id_classes

---
+++ LHL-Final-Project/server/db/seeds/04_student_id_classes.sql
@@ -0,0 +1,10 @@
+INSERT INTO student_id_class (student_id, class_id)
+VALUES (1, 1);
+
+INSERT INTO student_id_class (student_id, class_id)
+VALUES (2, 2);
+
+INSERT INTO student_id_class (student_id, class_id)
+VALUES (3, 3);
+
+
