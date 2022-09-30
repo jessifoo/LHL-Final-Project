db09994ef42aa898a91947e336fe872e845cc520
DohBae <bdoherty92@msn.com>
2022-09-30 15:33:11 +0000

    Seed comments

---
+++ LHL-Final-Project/server/db/seeds/08_comments.sql
@@ -0,0 +1,8 @@
+INSERT INTO comments (id, body, note_id, student_id )
+VALUES (1, "Good work!", 3, 1);
+
+INSERT INTO comments (id, body, note_id, student_id )
+VALUES (1, "Great notes! I alwayy find your notes so informative. It'd be really helpful if you can add headings", 1, 2);
+
+INSERT INTO comments (id, body, note_id, student_id )
+VALUES (3, "Wowee! Super helpful, thanks!", 2, 3);
