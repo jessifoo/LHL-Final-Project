911c00eeacb72bc60ff334ad175c405453aaad3e
DohBae <bdoherty92@msn.com>
2022-10-03 18:47:31 +0000

    Connect back end to front end

--- LHL-Final-Project/server/routes/indexRoutes.js
+++ LHL-Final-Project/server/routes/indexRoutes.js
@@ -1,11 +1,18 @@
+// routes that map to the same routes that are defined on the client side. deliniates how logic will be separated
+// Front end needs to talk to the same routes as the back end
+
 const router = require('express').Router();
 const users = require('../db/queries/users');
 
-router.get('/', (req, res) => {
+router.get('/users', (req, res) => {
   users.getAllUsers().then(data => {
     console.log(data);
     res.json({users: data});
   })
 });
+
+router.get('/register')
+
+
   
 module.exports = router;
