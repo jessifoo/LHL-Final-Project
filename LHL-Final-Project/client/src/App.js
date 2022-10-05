9f401c9f94e3bb582b43c273da031999886cb733
Netsanet <netsizebene@gmail.com>
2022-10-05 04:07:50 +0000

    add nav-bar and installed react-router

--- LHL-Final-Project/client/src/App.js
+++ LHL-Final-Project/client/src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import React from 'react';
 import User from "./components/User";
 import Notes from "./components/Notes";
+import NavBar from './components/NavBar';
+import AddNote from './components/AddNote';
 
 
 function App() {
 
   return (
     <div>
+      <NavBar />
+      <div className='content'>
+
       <User  />
       <Notes />
-
+      <AddNote/>
+      </div>
     </div>
   );
 }
---
+++ LHL-Final-Project/client/src/components/NavBar.js
@@ -0,0 +1,72 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css'
+
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
+
+export default function NavBar(props) {
+
+
+  return (
+    
+
+    <Navbar bg="color" variant ="dark" fixed="top" expand="lg">
+      <Navbar.Brand>
+      <img src= "https://cdn.vectorstock.com/i/1000x1000/45/29/cute-dog-flat-logo-icon-vector-32724529.webp" width="50" hight="20"/>
+      </Navbar.Brand>
+      <Navbar.Toggle/>
+      <Navbar.Collapse>
+
+      <Nav>
+        <Nav.Link href="#some"> Dashboard</Nav.Link>
+        <Nav.Link href=""> Add Notes</Nav.Link>
+        <Nav.Link href=""> Favorites</Nav.Link>
+        <NavDropdown title ="Classes"> 
+        <DropdownItem href=""> Math</DropdownItem>
+        <DropdownItem href=""> History</DropdownItem>
+        <DropdownItem href=""> Programing</DropdownItem>
+        </NavDropdown>
+        <Nav.Link> Logout</Nav.Link>
+      </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    
+  )
+
+  // return (
+  //   <nav>
+  //     <a href="/">
+  //        Logo
+  //        </a>
+  //        <ul>
+  //         <li>
+  //           <a href ="/dashboard">
+  //             Dashboard
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a href ="/post">
+  //             Post
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a href ="/favorites">
+  //             Favorites
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a href ="/classes">
+  //             Classes
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a href ="/logout">
+  //             Logout
+  //           </a>
+  //         </li>
+  //        </ul>
+
+  //   </nav>
+  // )
+
+}
